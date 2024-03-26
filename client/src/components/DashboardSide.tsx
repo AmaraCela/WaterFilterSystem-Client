7269baf49feda4ae9c759eaceb66cd9d4f53fb30
AmaraCela <amaracela@gmail.com>
2024-03-26 11:53:33 +0100

    init

--- client/src/components/DashboardSide.tsx
+++ client/src/components/DashboardSide.tsx
@@ -9,7 +9,7 @@ const bookmark = require("../assets/Bookmark.png");
 
 const DashboardSide = () => {
     return (
-        <div className="side w-1/3 h-full flex flex-col justify-between items-center py-6 rounded-3xl">
+        <div className="side w-1/3 h-full flex flex-col justify-between items-center py-8 rounded-3xl">
             <div className="w-full flex flex-col justify-center items-center">
                 <img src={profileImg} alt="Profile" className="w-28" />
                 <p className="main-font font-bold mt-2">Ediola Kola</p>
@@ -20,7 +20,7 @@ const DashboardSide = () => {
             <DashboardLink to={""} src={calendar} text={"Schedules"} />
             <DashboardLink to={""} src={chart} text={"Statistics"} />
             <DashboardLink to={""} src={danger} text={"Red List"} />
-            <DashboardLink to={""} src={paper} text={"Buyers and references"} />
+            <DashboardLink to={""} src={paper} text={"Buyers and References"} />
             <DashboardLink to={""} src={bookmark} text={"Collections"} />
         </div>
     );
