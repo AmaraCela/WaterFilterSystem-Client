c852397c499be63f842645028a3a8a0598e95ef1
AmaraCela <amaracela@gmail.com>
2024-04-21 13:25:51 +0200

    review meeting styled

--- client/src/pages/ReviewMeetings.tsx
+++ client/src/pages/ReviewMeetings.tsx
@@ -12,6 +12,7 @@ const ReviewMeetings = () => {
     const dispatch = useAppDispatch();
     const meetings = useSelector((state: RootState) => state.meeting.meetings);
     const [alertVisibility, setAlertVisibility] = useState('hidden');
+    const [editVisibility, setEditVisibility] = useState('hidden');
     const [meeting_id, setMeeting_id] = useState('');
 
     useEffect(() => {
@@ -37,12 +38,17 @@ const ReviewMeetings = () => {
                                 <div className="td-item"><div className="bg-[#5882c17d] rounded-[10px] p-2"><p className="w-full text-center">Actions</p></div></div>
                             </div>
                             {meetings.map((meeting) => (
-                                <div key={meeting.meeting_id} id={meeting.meeting_id.toString()} className="ml-4 flex w-full justify-evenly border rounded-md border-[#B8B2B2] bg-white">
+                                <div key={meeting.meeting_id} id={meeting.meeting_id.toString()} className="ml-4 flex w-full justify-evenly border rounded-md border-[#B8B2B2] bg-white mb-2">
                                     <div className="td-item flex items-center p-1"><p>{new Date(meeting.time).getHours()}:{new Date(meeting.time).getMinutes()}<br />{new Date(meeting.time).getDate()}/{new Date(meeting.time).getMonth()}/{new Date(meeting.time).getFullYear()}</p></div>
                                     <div className="td-item flex items-center p-1"><p>{meeting.place}</p></div>
                                     <div className="td-item flex items-center p-1"><p>{meeting.outcomeComment}</p></div>
                                     <div className="td-item flex items-center p-1"><p>{meeting.succesful}</p></div>
-                                    <div className="td-item flex items-center p-1"><div className="flex justify-evenly w-full"><button onClick={() => { setMeeting_id(meeting.meeting_id.toString()); setAlertVisibility("flex") }}><img src={deleteIcon} alt="" /></button> <button><img src={editIcon} alt="" /></button> </div></div>
+                                    <div className="td-item flex items-center p-1">
+                                        <div className="flex justify-evenly w-full">
+                                            <button onClick={() => { setMeeting_id(meeting.meeting_id.toString()); setAlertVisibility("flex") }}><img src={deleteIcon} alt="" /></button>
+                                            <button onClick={() => {setEditVisibility("flex");}}><img src={editIcon} alt="" /></button>
+                                        </div>
+                                    </div>
                                 </div>
                             ))}
                         </div>
@@ -50,11 +56,39 @@ const ReviewMeetings = () => {
                 </div>
             </div>
             <div className={`absolute h-full w-full top-0 ${alertVisibility} items-center justify-center`}>
-                <div className="h-1/3 w-1/3 z-10 bg-white alert-box rounded-md flex flex-col justify-between py-4">
-                    <p className="main-font w-full text-center mt-8 font-semibold text-lg">Proceed with deleting the meeting?</p>
+                <div className="border-2 border-[#844B2A] bg-white p-12 rounded-[30px]">
+                    <p className="main-font w-full text-center mt-8 font-semibold text-lg text-[#bb0000]">Proceed with deleting the meeting?</p>
                     <div className="flex w-full">
-                        <button className="bg-[#bb0000] main-font w-1/2 h-[35px] text-white m-3 rounded-md" onClick={() => { dispatch(deleteMeeting(meeting_id)); setAlertVisibility("hidden"); }}>Yes</button>
-                        <button className="bg-[#79f33d] main-font w-1/2 h-[35px] m-3 rounded-md" onClick={() => setAlertVisibility("hidden")}>Cancel</button>
+                        <button className="rounded-[4px] font-bold border-2 border-[#E97652] main-font w-1/2 h-[35px] m-3" onClick={() => { dispatch(deleteMeeting(meeting_id)); setAlertVisibility("hidden"); }}>Yes</button>
+                        <button className="rounded-[4px] font-bold border-2 border-[#E97652] main-font w-1/2 h-[35px] m-3 " onClick={() => setAlertVisibility("hidden")}>Cancel</button>
+                    </div>
+                </div>
+            </div>
+            <div className={`absolute h-full w-full top-0 ${editVisibility} items-center justify-center`}>
+                <div className="border-2 border-[#844B2A] bg-white p-12 rounded-[30px]">
+                    <div className="flex">
+                        <img src={editIcon} alt="" />
+                        <p className="montserrat text-[#844B2A] font-bold">edit meeting</p>
+                    </div>
+                    <label htmlFor="name" className="montserrat text-[#844B2A] font-semibold">Name of Client</label><br />
+                    <input type="text" id="name" className="border-2 rounded-md border-[#844B2A] bg-[#fd9e305c] w-full" />
+                    <br />
+                    <label htmlFor="address" className="montserrat text-[#844B2A] font-semibold">Address of Client</label><br />
+                    <input type="text" id="address" className="border-2 rounded-md border-[#844B2A] bg-[#fd9e305c] w-full" />
+                    <br />
+                    <label htmlFor="number" className="montserrat text-[#844B2A] font-semibold">Phone Number</label><br />
+                    <input type="text" id="number" className="border-2 rounded-md border-[#844B2A] bg-[#fd9e305c] w-full" />
+                    <br />
+                    <label htmlFor="date" className="montserrat text-[#844B2A] font-semibold">Date and Time</label><br />
+                    <input type="date" id="date" className="border-2 rounded-md border-[#844B2A] bg-[#fd9e305c]" />
+                    <input type="time" id="date" className="border-2 ml-4 rounded-md border-[#844B2A] bg-[#fd9e305c]" />
+                    <br />
+                    <label htmlFor="notes" className="montserrat text-[#844B2A] font-semibold">Special Notes (Optional)</label><br />
+                    <input type="text" id="notes" className="border-2 rounded-md border-[#844B2A] bg-[#fd9e305c] w-full" />
+                    <br />
+                    <div className="flex justify-evenly mt-6">
+                        <button className="rounded-[4px] font-bold border-2 border-[#E97652] w-1/3" onClick={()=>{setEditVisibility("hidden")}}>CANCEL</button>
+                        <button className="rounded-[4px] font-bold border-2 border-[#E97652] w-1/3">SAVE</button>
                     </div>
                 </div>
             </div>
