1de23f9fe0605a2245e483879a46a5ec3fc001d2
AmaraCela <amaracela@gmail.com>
2024-04-23 17:37:35 +0200

    dashboard fixes

--- client/src/components/DashboardSide_SalesAgent.tsx
+++ client/src/components/DashboardSide_SalesAgent.tsx
@@ -1,11 +1,11 @@
 import DashboardLink from "./DashboarLink";
 const profileImg = require("../assets/profileImg.png");
-const calling = require("../assets/Calling.png");
+const calling = require("../assets/Time_duotone_line.png");
 const calendar = require("../assets/Calendar.png");
-const chart = require("../assets/Chart.png");
+const log = require("../assets/Calendar_add_duotone.png");
 const danger = require("../assets/DangerTriangle.png");
-const paper = require("../assets/Paper.png");
-const bookmark = require("../assets/Bookmark.png");
+const insta = require("../assets/insta_duotone_line.png");
+const sales = require("../assets/Chart_fill.png");
 
 const DashboardSideSalesAgent = () => {
     return (
@@ -18,9 +18,9 @@ const DashboardSideSalesAgent = () => {
 
             <DashboardLink to={"workschedule"} src={calling} text={"Set Work schedule"} highlighted={false} />
             <DashboardLink to={""} src={calendar} text={"My Meetings"} highlighted={false} />
-            <DashboardLink to={""} src={calendar} text={"Log Meetings"} highlighted = {false}/>
-            <DashboardLink to={""} src={calendar} text={"References"} highlighted = {false}/>
-            <DashboardLink to={""} src={danger} text={"Chat"} highlighted = {false}/> 
+            <DashboardLink to={""} src={log} text={"Log Meetings"} highlighted = {false}/>
+            <DashboardLink to={""} src={insta} text={"References"} highlighted = {false}/>
+            <DashboardLink to={""} src={sales} text={"Sales"} highlighted = {false}/> 
         </div>
     );
 }
