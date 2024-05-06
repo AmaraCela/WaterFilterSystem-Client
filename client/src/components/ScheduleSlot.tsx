eaa7734b9c3960460d13fcbeb87486b6a84f3ad4
unknown <mmaksuti21@epoka.edu.al>
2024-05-06 20:42:54 +0200

    Properly display meeting times

--- client/src/components/ScheduleSlot.tsx
+++ client/src/components/ScheduleSlot.tsx
@@ -1,13 +1,24 @@
-const time = require("../assets/Vector.png");
-const ScheduleSlot = ({height, name, surname}: {height: string, name: string, surname: string}) => {
+const clockimg = require("../assets/Vector.png");
+const ScheduleSlot = ({height, name, surname, time}: {height: string, name: string, surname: string, time: string}) => {
+    const date = new Date(time);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const timeStart = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+
+    // assuming meeting is 90 minutes
+    date.setMinutes(date.getMinutes() + 90);
+    const hoursEnd = date.getHours();
+    const minutesEnd = date.getMinutes();
+    const timeEnd = `${hoursEnd.toString().padStart(2, '0')}:${minutesEnd.toString().padStart(2, '0')}`;
+
     return (
         <div className={`rounded-md pl-2 overflow-hidden bg-[#67E952] ${height}`}>
             <div className="bg-[#E9EFFF] pl-2 h-full">
                 <p className="montserrat font-medium text-sm text-[#5272E9]">{name} {surname}</p>
                 {/* <p className="montserrat font-medium text-[#736A6A] text-xs" >Client: <p className="inline underline">Erion Lama</p></p> */}
                 <div className="flex">
-                    <img src={time} alt="" />
-                    <p className="montserrat text-xs inline text-[#5272E9] pl-1">09:00 - 10:30</p>
+                    <img src={clockimg} alt="" />
+                    <p className="montserrat text-xs inline text-[#5272E9] pl-1">{timeStart} - {timeEnd}</p>
                 </div>
             </div>
         </div>
